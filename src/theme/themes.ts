058383566ff5c958d45d59e1228d7e81ae3a2ff7
Cristiano Pereira da Silva <102186472+cristianoprogramador@users.noreply.github.com>
2024-07-26 15:08:19 -0300

    first commit

---
+++ src/theme/themes.ts
@@ -0,0 +1,12 @@
+"use client";
+
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "Nanito, sans-serif",
+  },
+});
+
+
+export default theme;
